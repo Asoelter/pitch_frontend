5af11cfd75b1ef2fa1c1d58fc2faf9438c9c9ec3
Adam <adamdlsoelter@gmail.com>
2020-11-24 20:19:26 -0600

    added wui as a subproject

--- pitch_frontend/WinMain.cpp
+++ pitch_frontend/WinMain.cpp
@@ -2,10 +2,16 @@
 #   define WIN32_LEAN_AND_MEAN
 #endif
 
+#ifndef NOMINMAX
+#   define NOMINMAX
+#endif
+
 #include <string>
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+
+#include <projectY/gui/window.h>
 
 #include <covid/socket.h>
 
@@ -19,12 +25,21 @@ INT WinMain(HINSTANCE hInstance,
     PSTR lpCmdLine,
     INT nCmdShow)
 {
+#if 0
     const auto socket = Socket::connectTo(Port("127.0.0.1", "49152"));
     socket.send({'t', 'e', 's', 't', '\0'});
 
     const auto messageInBytes = socket.receive();
     const auto message = std::string(messageInBytes.begin(), messageInBytes.end());
     MsgBox(message.c_str(), "received");
+#endif
+
+    auto window = gui::Window(gui::WindowRect(700, 400), "Pitch");
+
+    while(window.isOpen())
+    {
+        window.update();
+    }
 
     return 0;
 }
